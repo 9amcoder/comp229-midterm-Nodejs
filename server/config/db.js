ea356c2cac113f0412198f0032378d091a91c3a8
Steve Sultan <stevesultan@outlook.com>
2020-10-30 12:43:28 -0400

    initial commit

---
+++ server/config/db.js
@@ -0,0 +1,6 @@
+module.exports = {
+  //local MongoDB deployment ->
+  //"URI": "mongodb://localhost/books229"
+  //"URI":mongodb+srv://user01:<password>@cluster0.cc8fo.mongodb.net/<dbname>?retryWrites=true&w=majority
+  "URI": "mongodb+srv://user01:comp229@cluster0.cc8fo.mongodb.net/book_store?retryWrites=true&w=majority"
+}
